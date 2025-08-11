aa03499560ff7cac8f8fad74d696f0461f452011
Yeon <yeonsujin97@gmail.com>
2025-08-11 13:59:30 +0900

    0811 - 2

--- Vispect/Setting/CommunicatorSetting.Designer.cs
+++ Vispect/Setting/CommunicatorSetting.Designer.cs
@@ -64,6 +64,7 @@
             this.btnApply.TabIndex = 18;
             this.btnApply.Text = "적용";
             this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
             // 
             // txtIpAddr
             // 
